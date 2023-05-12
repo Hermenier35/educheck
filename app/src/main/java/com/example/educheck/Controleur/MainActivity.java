a98538a9cedf19dff52e1ba431c5f91173b3e90b
issa-hassa <75831785+issa-hassa@users.noreply.github.com>
2023-05-12 16:10:59 +0200

    login_activity

--- app/src/main/java/com/example/educheck/Controleur/MainActivity.java
+++ app/src/main/java/com/example/educheck/Controleur/MainActivity.java
@@ -2,15 +2,29 @@ package com.example.educheck.Controleur;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.educheck.R;
 
 public class MainActivity extends AppCompatActivity {
+    Intent login_activity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        login_activity = new  Intent(MainActivity.this,login.class);
+        Button login = findViewById(R.id.button);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(login_activity);
+            }
+        });
     }
+
 }
