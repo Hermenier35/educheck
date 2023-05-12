ee8f5d8a7c6e4774c9544b301a7e6bc720b98f08
Pierre-Edouard <Pierre Edouard Hermeniepierre-edouard.hermenier@etudiant.univ-rennes1.fr>
2023-05-12 17:11:42 +0200

    implementation interface login + modification class Student et Teacher hérite de Users

---
+++ app/src/main/java/com/example/educheck/Modele/LoginImplementation.java
@@ -0,0 +1,14 @@
+package com.example.educheck.Modele;
+
+public class LoginImplementation implements Login {
+
+    @Override
+    public boolean forgetPassword(String mail) {
+        return false;
+    }
+
+    @Override
+    public String connexion(String mail, String password) {
+        return null;
+    }
+}
