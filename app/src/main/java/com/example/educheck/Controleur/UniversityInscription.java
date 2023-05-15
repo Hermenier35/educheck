7fbd3ea498bf2f6ee5bc6dc65e8d8b0669c71498
mberkaoui <meryem.berkaoui@etudiant.univ-rennes1.fr>
2023-05-15 17:27:31 +0200

    list_universities_final3

--- app/src/main/java/com/example/educheck/Controleur/UniversityInscription.java
+++ app/src/main/java/com/example/educheck/Controleur/UniversityInscription.java
@@ -1,35 +1,26 @@
 package com.example.educheck.Controleur;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.Toast;
+
 
 import com.example.educheck.Modele.Implementation.InscriptionImplementation;
 import com.example.educheck.Modele.Interface.AsyncTaskcallback;
 import com.example.educheck.Modele.University;
 import com.example.educheck.R;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
+
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
 
 public class UniversityInscription extends AppCompatActivity implements AsyncTaskcallback {
 
@@ -54,7 +45,7 @@ public class UniversityInscription extends AppCompatActivity implements AsyncTas
     public void onTaskCompleted(JSONArray items) throws JSONException {
         for(int i = 0; i < items.length(); i++){
             JSONObject uniJson = items.getJSONObject(i);
-            University university = new University(uniJson.getString("name"), "suffixe");
+            University university = new University(uniJson.getString("name"), uniJson.getString("suffixe"));
             Button button = new Button(this);
             button.setText(university.getName());
             layout.addView(button);
