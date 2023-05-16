a5a1eaec3fd2173ede6f6702d5cb26c47f66c45e
vahid <vahid.tourang@etudiant.univ-rennes1.fr>
2023-05-16 17:03:34 +0200

    student dashboard v1: 1) waiting for confirmation 2) dashboard with acces to the courses and msg 3) marks 4) plan 5)justification absence ------- icons size is corrected

---
+++ app/src/main/java/com/example/educheck/Controleur/Marks.java
@@ -0,0 +1,16 @@
+package com.example.educheck.Controleur;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.educheck.R;
+
+public class Marks extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_marks);
+    }
+}
