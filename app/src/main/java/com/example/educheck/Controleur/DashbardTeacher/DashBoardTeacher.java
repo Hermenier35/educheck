f8358e6bb6ecbdc46f20078fc25e4f88b0d713cd
mberkaoui <meryem.berkaoui@etudiant.univ-rennes1.fr>
2023-06-01 16:56:16 +0200

    DashBoard_Teacher : frag addStudent v2

---
+++ app/src/main/java/com/example/educheck/Controleur/DashbardTeacher/DashBoardTeacher.java
@@ -0,0 +1,63 @@
+package com.example.educheck.Controleur.DashbardTeacher;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.view.View;
+import android.widget.Button;
+
+import com.example.educheck.Controleur.Dashboard.Frag1;
+import com.example.educheck.Controleur.Dashboard.Frag2;
+import com.example.educheck.Controleur.Dashboard.Frag3;
+import com.example.educheck.R;
+
+public class DashBoardTeacher extends AppCompatActivity {
+    Button btn1, btn2,btn3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard_prof);
+
+        btn1 = findViewById(R.id.tmenu1);
+        btn2 = findViewById(R.id.tmenu2);
+        btn3 = findViewById(R.id.tmenu3);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        replaceFragment(new Frag1());
+                                    }
+                                }
+        );
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        replaceFragment(new Frag2());
+                                    }
+                                }
+        );
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        replaceFragment(new Frag3());
+                                    }
+                                }
+        );
+
+
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction =  fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameTeacher,fragment);
+        fragmentTransaction.commit();
+    }
+}
