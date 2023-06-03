411aa6e08afe2dedc0e75cc9a98111134ad96e94
El Mellali Yasser <yasser.elmellali@etudiant.univ-rennes1.fr>
2023-06-03 15:01:43 +0200

    FragmentMessage1 marche , on peut choisir à qui envoyer la mail.

--- app/src/main/java/com/example/educheck/Controleur/Dashboard/DashBoardEtudiant.java
+++ app/src/main/java/com/example/educheck/Controleur/Dashboard/DashBoardEtudiant.java
@@ -50,7 +50,7 @@ public class DashBoardEtudiant extends AppCompatActivity {
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                replaceFragment(new Frag2());
+                replaceFragment(new FragMessages1());
             }
         }
         );
@@ -58,7 +58,7 @@ public class DashBoardEtudiant extends AppCompatActivity {
         btn3.setOnClickListener(new View.OnClickListener() {
                                     @Override
                                     public void onClick(View v) {
-                                        replaceFragment(new Frag3());
+                                        replaceFragment(new FragMessages2());
                                     }
                                 }
         );
--- app/src/main/java/com/example/educheck/Controleur/Dashboard/FragMessages1.java
+++ app/src/main/java/com/example/educheck/Controleur/Dashboard/FragMessages1.java
@@ -1,24 +1,21 @@
 package com.example.educheck.Controleur.Dashboard;
 
-
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Adapter;
+import android.view.ViewGroup;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.educheck.Controleur.Registration.Registration1;
-import com.example.educheck.Controleur.Registration.UnivAdapterCard;
-import com.example.educheck.Controleur.Registration.UniversityInscription;
 import com.example.educheck.Modele.Implementation.DashboardImplementation;
-import com.example.educheck.Modele.Implementation.InscriptionImplementation;
 import com.example.educheck.Modele.Interface.AsyncTaskcallback;
-import com.example.educheck.Modele.University;
-import com.example.educheck.Modele.Users;
 import com.example.educheck.R;
 
 import org.json.JSONArray;
@@ -26,70 +23,83 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.Base64;
 import java.util.Iterator;
 import java.util.logging.Logger;
 
-public class FragMessages1 extends AppCompatActivity implements AsyncTaskcallback {
+public class FragMessages1 extends Fragment implements AsyncTaskcallback {
     private Intent intentMess1;
     private DashboardImplementation messageImplementation;
     private RecyclerView.LayoutManager layoutManager;
     public static View.OnClickListener myOnClickListener;
     private RecyclerView recyclerView;
-    private RecyclerView.Adapter adpater_card;
-    private ArrayList<String>  users_mail ;
+    private RecyclerView.Adapter adapter_card;
+    private ArrayList<String> users_mail;
 
     private String token;
 
+    @SuppressLint("MissingInflatedId")
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        Logger.getGlobal().info("University itent init");
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_university_inscription);
-        intentMess1 = new Intent(getApplicationContext(), FragMessages2.class);
-        layoutManager = new LinearLayoutManager(this);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Logger.getGlobal().info("Message itent init");
+        View view = inflater.inflate(R.layout.fragment_messages1, container, false);
+
+        intentMess1 = new Intent(getContext(), FragMessages2.class);
+        layoutManager = new LinearLayoutManager(getContext());
         users_mail = new ArrayList<>();
-        token = getIntent().getStringExtra("token");
-        myOnClickListener = new FragMessages1.MyOnClickListener(this, recyclerView);
-        recyclerView = findViewById(R.id.recycler);
+        token = getActivity().getIntent().getStringExtra("token");
+        myOnClickListener = new FragMessages1.MyOnClickListener(getContext(), recyclerView);
+
+        recyclerView = view.findViewById(R.id.recycler_mex);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(layoutManager);
+
         messageImplementation = new DashboardImplementation(this);
         messageImplementation.sendMexTo(token);
+
+        adapter_card = new MailAdapterCard(users_mail);
+        recyclerView.setAdapter(adapter_card);
+
+        return view;
     }
 
     @Override
     public void onTaskCompleted(JSONArray items) throws JSONException {
-        if(items.getJSONObject(0).has("code_retour"))
+        if (items.getJSONObject(0).has("code_retour"))
             System.out.println("code_retour: " + items.getJSONObject(0).get("code_retour"));
-        for(int i = 0; i < items.length(); i++){
-            JSONObject mailJson = items.getJSONObject(i);
-            System.out.println("value de i = " + i);
-            Iterator<String> it = mailJson.keys();
-            while(it.hasNext())
-                System.out.println(it.next());
-            String mail =mailJson.getString("mail");
-            users_mail.add(mail);
-        }
-        adpater_card = new MailAdapterCard(users_mail);
-        recyclerView.setAdapter(adpater_card);
+            JSONObject mailJson = items.getJSONObject(0);
+            String mail = mailJson.getString("mail").replaceAll("[\\[\\]\" ]" , "");
+            String[] mails= mail.split(",");
+            for (int j=0;j<mails.length;j++) {
+                users_mail.add(mails[j]);
+            }
 
 
+        adapter_card = new MailAdapterCard(users_mail);
+        recyclerView.setAdapter(adapter_card);
     }
 
-
-    private class MyOnClickListener implements View.OnClickListener{
+    private class MyOnClickListener implements View.OnClickListener {
         private Context context;
         private RecyclerView recyclerView;
-        public MyOnClickListener(Context context, RecyclerView recyclerView){
+
+        public MyOnClickListener(Context context, RecyclerView recyclerView) {
             this.context = context;
             this.recyclerView = recyclerView;
         }
+
         @Override
         public void onClick(View v) {
             String mailRecipient = users_mail.get(v.getVerticalScrollbarPosition());
             intentMess1.putExtra("mailRecipient",mailRecipient);
-            startActivity(intentMess1);
+            // startActivity(intentMess1);
+            replaceFragment(new FragMessages2());
         }
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame1, fragment);
+        fragmentTransaction.commit();
     }
 }
