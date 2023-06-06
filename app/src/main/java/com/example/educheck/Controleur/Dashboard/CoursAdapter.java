01f1261948de2ea8ba64f76ce82d6d96e5bd20a1
vahid <vahid.tourang@etudiant.univ-rennes1.fr>
2023-06-06 09:18:59 +0200

    FragGetCourses print problem modify button recyleView et CoursAdapter

--- app/src/main/java/com/example/educheck/Controleur/Dashboard/CoursAdapter.java
+++ app/src/main/java/com/example/educheck/Controleur/Dashboard/CoursAdapter.java
@@ -24,7 +24,7 @@ public class CoursAdapter extends RecyclerView.Adapter<CoursAdapter.ViewHolder> 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_cours, parent, false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_get_cours, parent, false);
         return new ViewHolder(view);
     }
 
