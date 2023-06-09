a9cb7a1ffec5b90061a398fc94fef6a27d35d53b
Pierre-Edouard <Pierre Edouard Hermeniepierre-edouard.hermenier@etudiant.univ-rennes1.fr>
2023-06-09 16:40:54 +0200

    for change

--- app/src/main/java/com/example/educheck/Utils/HttpUrl.java
+++ app/src/main/java/com/example/educheck/Utils/HttpUrl.java
@@ -1,7 +1,7 @@
 package com.example.educheck.Utils;
 
 public class HttpUrl {
-    private static String ip = "http://10.188.217.30:3000/api/";
+    private static String ip = "http://10.41.50.177:3000/api/";
     public static String UrlPostOnUniversity = ip + "addUser";
     public static String UrlGetUniversity = ip + "getUniversity";
     public static String UrlPostUniversity = ip + "addUni";
@@ -21,7 +21,7 @@ public class HttpUrl {
     public static String UrlPersonalCourses = ip + "";
     public static String UrlEditUniversity = ip + "editUniversity";
     public static String UrlAddCourse = ip + "setCourses";
-    public static String UrlPostCourse = ip + "postCoursesStudent";
+    public static String UrlPostCourseStudent = ip + "postCoursesStudent";
     public static String UrlGetMarks= ip + "getNotes";
 
 }
